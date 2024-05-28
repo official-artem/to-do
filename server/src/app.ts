d404cbf48e75ba1a0846c92531cccc811399991f
Artem Hrechuk <hrechuk.artem@gmail.com>
2024-05-28 17:46:37 +0300

    added .env.example and imported to project mongoose

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
-const app = express();
+
 const port = process.env.PORT ?? 3000;
+const app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
