f1161932665d760dd88fcb5ad6641b5676f656dd
Artem Hrechuk <hrechuk.artem@gmail.com>
2024-05-29 13:54:58 +0300

    added initial routes

---
+++ server/src/routes/todo.route.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  return '';
+});
+
+router.get('/:id', (req, res) => {
+  return '';
+});
+
+router.post('/', (req, res) => {
+  return '';
+});
+
+router.delete('/:id', (req, res) => {
+  return '';
+});
+
+export default router;
+
---
+++ server/src/routes/user.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  return ''
+})
+
+router.post('/', (req, res) => {
+  return ''
+})
+
+router.delete('/:id', (req, res) => {
+  return ''
+})
+
+export default router;
+
